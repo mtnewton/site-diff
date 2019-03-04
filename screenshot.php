e9bf0e0636af557621304d7e32c8a8437ef6babd
Marvin Newton <marvin.newton@traderinteractive.com>
2019-03-04 15:23:21 -0500

    ignore https errors

--- screenshot.php
+++ screenshot.php
@@ -6,14 +6,26 @@ use Spatie\Browsershot\Browsershot;
 
 $outputPath = __DIR__ . '/output';
 
-Browsershot::url($argv[1])->noSandbox()->fullPage()->waitUntilNetworkIdle()->delay(1000)->timeout(999)->save($outputPath . '/A.png');
-Browsershot::url($argv[2])->noSandbox()->fullPage()->waitUntilNetworkIdle()->delay(1000)->timeout(999)->save($outputPath . '/B.png');
+Browsershot::url($argv[1])
+    ->noSandbox()
+    ->fullPage()
+    ->waitUntilNetworkIdle()
+    ->delay(1000)
+    ->timeout(999)
+    ->ignoreHttpsErrors()
+    ->save($outputPath . '/A.png');
+
+Browsershot::url($argv[2])
+    ->noSandbox()
+    ->fullPage()
+    ->waitUntilNetworkIdle()
+    ->delay(1000)
+    ->timeout(999)
+    ->ignoreHttpsErrors()
+    ->save($outputPath . '/B.png');
 
 $diff = exec("compare -metric mae -compose src $outputPath/A.png $outputPath/B.png $outputPath/Diff.png 2>&1", $_, $status);
-
-
 
 if ($status) {
     trigger_error('Sites are different or there was an error: ' . $diff, E_USER_ERROR);
 }
-
